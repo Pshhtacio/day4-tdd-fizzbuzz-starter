23683df27de07666f6794ec7367999f2eec5dcdd
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-17 16:21:49 +0800

    Test: add test for multiple of 3 and 7

--- src/test/java/com/afs/tdd/FizzBuzzTest.java
+++ src/test/java/com/afs/tdd/FizzBuzzTest.java
@@ -1,14 +1,14 @@
 package com.afs.tdd;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
     FizzBuzz fizzBuzz = new FizzBuzz();
+
     @Test
-    void should_return_the_given_number_when_countOff_given_a_normal_number(){
+    void should_return_the_given_number_when_countOff_given_a_normal_number() {
         //Given
         int normalNumber = 1;
         //When
@@ -18,27 +18,36 @@ public class FizzBuzzTest {
     }
 
     @Test
-    void should_return_Fizz_when_countOff_given_a_number_multiple_of_3(){
+    void should_return_Fizz_when_countOff_given_a_number_multiple_of_3() {
         int numberMultipleOf3 = 3;
         String result = fizzBuzz.countOff(numberMultipleOf3);
         assertEquals("Fizz", result);
     }
+
     @Test
-    void should_return_Buzz_when_countOff_given_a_number_multiple_of_5(){
+    void should_return_Buzz_when_countOff_given_a_number_multiple_of_5() {
         int numberMultipleOf5 = 5;
         String result = fizzBuzz.countOff(numberMultipleOf5);
         assertEquals("Buzz", result);
     }
+
     @Test
-    void should_return_FizzBuzz_when_countOff_given_a_number_multiple_of_3_and_5(){
-        int numberMultipleOf15 = 15;
-        String result = fizzBuzz.countOff(numberMultipleOf15);
+    void should_return_FizzBuzz_when_countOff_given_a_number_multiple_of_3_and_5() {
+        int numberMultipleOf3And5 = 15;
+        String result = fizzBuzz.countOff(numberMultipleOf3And5);
         assertEquals("FizzBuzz", result);
     }
+
     @Test
-    void should_return_Whizz_when_countOff_given_a_number_multiple_of_7(){
+    void should_return_Whizz_when_countOff_given_a_number_multiple_of_7() {
         int numberMultipleOf7 = 7;
         String result = fizzBuzz.countOff(numberMultipleOf7);
         assertEquals("Whizz", result);
+    }
+    @Test
+    void should_return_Whizz_when_countOff_given_a_number_multiple_of_3_and_7() {
+        int numberMultipleOf3And7 = 21;
+        String result = fizzBuzz.countOff(numberMultipleOf3And7);
+        assertEquals("FizzWhizz", result);
     }
 }
