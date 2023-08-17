a4d9e3024fc0df6f5d19a752c2be22b5fa071203
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-17 15:33:54 +0800

    test: add test should_return_Fizz_when_countOff_given_a_number_multiple_of_3

--- src/test/java/com/afs/tdd/FizzBuzzTest.java
+++ src/test/java/com/afs/tdd/FizzBuzzTest.java
@@ -3,19 +3,26 @@ package com.afs.tdd;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class FizzBuzzTest {
+    FizzBuzz fizzBuzz = new FizzBuzz();
     @Test
     void should_return_the_given_number_when_countOff_given_a_normal_number(){
         //Given
         int normalNumber = 1;
-        FizzBuzz fizzBuzz = new FizzBuzz();
         //When
         String result = fizzBuzz.countOff(normalNumber);
         //Then
-        Assertions.assertEquals("1", result);
+        assertEquals("1", result);
     }
 
-    void should_return_Fizz_when_countOff_given_a_number_multiple_of_3(){}
+    @Test
+    void should_return_Fizz_when_countOff_given_a_number_multiple_of_3(){
+        int numberMultipleOf3 = 3;
+        String result = fizzBuzz.countOff(numberMultipleOf3);
+        assertEquals("Fizz", result);
+    }
     void should_return_Buzz_when_countOff_given_a_number_multiple_of_5(){}
     void should_return_FizzBuzz_when_countOff_given_a_number_multiple_of_15(){}
 }
