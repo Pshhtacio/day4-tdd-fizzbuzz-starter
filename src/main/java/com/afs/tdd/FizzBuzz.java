4768fdba7ffacc49f72f98f12bf14f8df4ee20f6
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-17 15:32:30 +0800

    feat: define countOff based on test should_return_the_given_number_when_countOff_given_a_normal_number

--- src/main/java/com/afs/tdd/FizzBuzz.java
+++ src/main/java/com/afs/tdd/FizzBuzz.java
@@ -4,4 +4,8 @@ public class FizzBuzz {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public String countOff(int normalNumber) {
+        return Integer.toString(normalNumber);
+    }
 }
