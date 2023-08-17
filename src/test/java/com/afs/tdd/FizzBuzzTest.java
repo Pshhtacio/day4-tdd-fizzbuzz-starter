359e87ac26b810bc06bbe56b3db5109fc236134d
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-17 15:18:27 +0800

    test: add test case  should_return_the_given_number_when_countOff_given_a_normal_number

--- src/test/java/com/afs/tdd/FizzBuzzTest.java
+++ src/test/java/com/afs/tdd/FizzBuzzTest.java
@@ -1,5 +1,17 @@
 package com.afs.tdd;
 
-public class FizzBuzzTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+public class FizzBuzzTest {
+    @Test
+    void should_return_the_given_number_when_countOff_given_a_normal_number(){
+        //Given
+        int normalNumber = 1;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        //When
+        String result = fizzBuzz.countOff(normalNumber);
+        //Then
+        Assertions.assertEquals("1", result);
+    }
 }
