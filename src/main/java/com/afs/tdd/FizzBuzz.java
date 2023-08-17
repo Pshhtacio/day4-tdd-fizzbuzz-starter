ee0f900d74da994152ba5a91ccde67e9d36c6a72
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-17 16:01:30 +0800

    refactor: FizzBuzz class

--- src/main/java/com/afs/tdd/FizzBuzz.java
+++ src/main/java/com/afs/tdd/FizzBuzz.java
@@ -1,28 +1,25 @@
 package com.afs.tdd;
 
 public class FizzBuzz {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-
     public String countOff(int number) {
         StringBuilder message = new StringBuilder();
         if (!isMultipleOf3(number) && !isMultipleOf5(number)) {
-            message.append(Integer.toString(number));
+            message.append(number);
         }
-        if(isMultipleOf3(number)){
+        if (isMultipleOf3(number)) {
             message.append("Fizz");
         }
-        if (isMultipleOf5(number)){
+        if (isMultipleOf5(number)) {
             message.append("Buzz");
         }
         return message.toString();
     }
 
-    public boolean isMultipleOf3(int number){
-        return number%3 == 0;
+    public boolean isMultipleOf3(int number) {
+        return number % 3 == 0;
     }
-    public boolean isMultipleOf5(int number){
-        return number%5 == 0;
+
+    public boolean isMultipleOf5(int number) {
+        return number % 5 == 0;
     }
 }
